a51ba6ae7eaf807196dc7188c29a2dc58603683a
Pankaj Tyagi <pankaj6255dav@gmail.com>
2024-11-19 15:09:33 +0530

    Add html for filter aside bar

--- pages/product.php
+++ pages/product.php
@@ -20,7 +20,6 @@
               $category = $row["category"];
               echo "<li><a href='../pages/product.php?category=$category'>$category</a></li>";
             }
-            mysqli_close($conn);
           ?>
           <li>
             <a href="../pages/product.php">More</a>
@@ -57,8 +56,48 @@
     ?>
 
     <div class="content">
-    <aside>Filter</aside>
-    <main>Products</main>
+      <aside class="product-filter">
+        <h2>Filter products</h2>
+        <form onsubmit="filter(event)">
+          <div class="filter-section">
+            <h3>Category:</h3>
+            <?php
+              $sql = "SELECT DISTINCT category FROM product";
+              $result = mysqli_query($conn, $sql);
+              while($row = mysqli_fetch_assoc($result)) {
+                $category = $row["category"];
+                echo "<label><input type='radio' name='category' value='$category'>$category</label><br>";
+              }
+              mysqli_close($conn);
+            ?>
+          </div>
+
+          <div class="filter-section">
+            <h3>Price:</h3>
+            <label><input type='radio' name='price' value='10000'>0 - 10,000</label><br>
+            <label><input type='radio' name='price' value='20000'>10,000 - 20,000</label><br>
+            <label><input type='radio' name='price' value='30000'>20,000 - 30,000</label><br>
+            <label><input type='radio' name='price' value='40000'>30,000 - 40,000</label><br>
+            <label><input type='radio' name='price' value='50000'>40,000 - 50,000</label><br>
+            <label><input type='radio' name='price' value='50000+'>50,000+</label>
+          </div>
+
+          <div class="filter-section">
+            <h3>Ratings:</h3>
+            <label><input type='radio' id='4.5' name='rating' value='4.5'>4.5+</label><br>
+            <label><input type='radio' id='4' name='rating' value='4'>4+</label><br>
+            <label><input type='radio' id='3.5' name='rating' value='3.5'>3.5+</label><br>
+            <label><input type='radio' id='3' name='rating' value='3'>3+</label><br>
+            <label><input type='radio' id='2' name='rating' value='2'>2+</label>
+          </div>
+
+          <div class="filter-buttons">
+            <button type="submit" class="apply-filters">Apply Filters</button>
+            <button type="reset" class="reset-filters">Reset</button>
+          </div>
+        </form>
+      </aside>
+    <main class="products"></main>
     </div>
 
     <script>
@@ -85,6 +124,11 @@
           }
         }
       })
+
+      const filter = event => {
+        event.preventDefault();
+        console.log(event);
+      }
     </script>
   </body>
 </html>
