30fc89c79c3de9052c60195ef5b86d00fef5d265
Pankaj Tyagi <pankaj6255dav@gmail.com>
2024-11-23 19:51:08 +0530

    Revert back changes

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -11,44 +11,9 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SPAM</title>
-    <link rel="icon" href="../images/favicon.ico">
-    <link rel="stylesheet" href="../css/header.css">
+    <title>Dashboard</title>
 </head>
 <body>
-<header>
-      <nav>
-        <ul id="left">
-          <?php
-            $conn = mysqli_connect("localhost", "root", "", "spam");
-            $sql = "SELECT category, count(pid) AS num_pid FROM product GROUP BY category ORDER BY num_pid DESC LIMIT 3";
-            $result = mysqli_query($conn, $sql);
-            while($row = mysqli_fetch_assoc($result)) {
-              $category = $row["category"];
-              echo "<li><a href='../pages/product.php?category=$category'>$category</a></li>";
-            }
-          ?>
-          <li>
-            <a href="../pages/product.php">More</a>
-          </li>
-        </ul>
-        <div class="main">
-          <b>Spam</b><img src="../images/spam.png" alt="SPAM logo">
-        </div>
-        <ul id="right">
-          <li style="display: flex;">
-            <input type="search" name="search" placeholder="Search item">
-            <img id="search" class="icon" src="../images/search.png" alt="Search icon">
-          </li>
-          <li>
-            <a href="../pages/dashboard.php"><img class="icon" src="../images/user.png" alt="User icon"></a>
-          </li>
-          <li>
-            <a href="../pages/cart.php"><img class="icon" src="../images/shopping-cart.png" alt="Cart icon"></a>
-          </li>
-        </ul>
-      </nav>
-    </header>
     <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
     <a href="logout.php">Logout</a>
 </body>
